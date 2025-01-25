8737bff2595cc8ae0bc5d6d75ca6b7ae4af7d6a1
Powell Lin <xinjun.lin@vts.com>
2025-01-24 21:09:21 -0500

    added toggle comment

--- .config/nvim/lua/commentator.lua
+++ .config/nvim/lua/commentator.lua
@@ -39,27 +39,70 @@ autocmd({"BufEnter", "BufRead", "BufNewFile"}, {
 	end
 })
 
-local function CommentLine()
-	vim.cmd(":silent! s@\\(\\S\\)@" .. startComment .. "\\1@")
-	vim.cmd(":silent! s@$@" .. endComment .. "@")
-end
-local function UncommentLine()
-	vim.cmd(":silent! s@" .. startComment .. "@@")
-	vim.cmd(":silent! s@$" .. endComment .. "@@")
+local function ToggleLine(force)
+  vim.fn.cursor({0, 0})
+
+  local line = vim.fn.line(".")
+  local commented = vim.fn.search("^\\s*" .. startComment, "cn", line) ~= 0
+  if force then
+    commented = false
+  end
+
+  if commented then
+    vim.cmd(":silent! s@" .. startComment .. "@@")
+    vim.cmd(":silent! s@$" .. endComment .. "@@")
+  else
+    vim.cmd(":silent! s@\\(\\S\\)@" .. startComment .. "\\1@")
+    vim.cmd(":silent! s@$@" .. endComment .. "@")
+  end
 end
 
-local function CommentLines()
-	vim.cmd(":silent! '<,'>s@\\(\\S\\)@" .. startComment .. "\\1@")
-	vim.cmd(":silent! '<,'>s@$@" .. endComment .. "@")
+local function ToggleRange(force)
+  local startLine = vim.fn.line("v")
+  local endLine = vim.fn.line(".")
+  local flags = "cn"
+  local reversed = startLine > endLine
+
+  if reversed then
+    startLine, endLine = endLine, startLine
+  end
+
+  vim.fn.cursor({startLine, 0})
+
+  local commented = vim.fn.search("^\\s*" .. startComment, flags, endLine) ~= 0
+  if force then
+    commented = false
+  end
+
+  -- set visual mode marks
+  vim.api.nvim_buf_set_mark(0, "<", startLine, 0, {})
+  vim.api.nvim_buf_set_mark(0, ">", endLine, 0, {})
+
+  if commented then
+    vim.cmd(":silent! '<,'>s@" .. startComment .. "@@")
+    vim.cmd(":silent! '<,'>s@$" .. endComment .. "@@")
+  else
+    vim.cmd(":silent! '<,'>s@\\(\\S\\)@" .. startComment .. "\\1@")
+    vim.cmd(":silent! '<,'>s@$@" .. endComment .. "@")
+  end
+
+  if reversed then
+    vim.fn.cursor({startLine, 0})
+  end
 end
 
-local function UncommentLines()
-	vim.cmd(":silent! '<,'>s@" .. startComment .. "@@")
-	vim.cmd(":silent! '<,'>s@$" .. endComment .. "@@")
+local function ToggleComments(force)
+  if vim.fn.mode() == "n" then
+    ToggleLine(force)
+  else
+    ToggleRange(force)
+  end
 end
 
-M.CommentLine = CommentLine
-M.UncommentLine = UncommentLine
-M.CommentLines = CommentLines
-M.UncommentLines = UncommentLines
+local function ForceComment()
+  ToggleComments(true)
+end
+
+M.ToggleComments = ToggleComments
+M.ForceComment = ForceComment
 return M
